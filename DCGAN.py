877d468ae4e03ac9c44840db6548c75e01a523f5
bakuratozoku <tsukuba.univer@gmail.com>
2017-08-20 22:11:41 +0900

    Update DCGAN.py

--- DCGAN.py
+++ DCGAN.py
@@ -8,17 +8,14 @@ import numpy as np
 import tensorflow as tf
 import os
 import matplotlib.pyplot as plt
-import scipy.misc
 import time
 
 
 z = tf.placeholder(tf.float32, [64, 100])
-
 image = tf.placeholder(tf.float32, [64, 64, 64, 3])
 
 
 sample_z = np.random.uniform(-1, 1, size=(64, 100))
-
 batch_z = np.random.uniform(-1, 1, [64, 100])
 
 
@@ -33,21 +30,21 @@ def batch_norm(x, name, train=True):
                       is_training=train,
                       scope=name)
 
-   
-   
+
 def conv2d(input_, output_dim, name="conv2d"):
     with tf.variable_scope(name):
         w = tf.get_variable('w', [5, 5, input_.get_shape()[-1], output_dim], initializer=tf.truncated_normal_initializer(stddev=0.02))
         conv = tf.nn.conv2d(input_, w, strides=[1, 2, 2, 1], padding='SAME')
         biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
     return tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
+
+
         
-    
-    
 def lrelu(x, name="lrelu"):
     return tf.maximum(x, 0.2*x)
 
- 
+
+
 def linear(input_, output_size,scope=None, with_w=False):
     shape = input_.get_shape().as_list()
     with tf.variable_scope(scope or "Linear"):
@@ -58,8 +55,8 @@ def linear(input_, output_size,scope=None, with_w=False):
         else:
             return tf.matmul(input_, matrix) + bias
 
-         
-         
+
+
 def linear_d(input_, output_size,scope=None, with_w=False):
     shape = input_.get_shape().as_list()
     with tf.variable_scope(scope or "Linear"):
@@ -70,8 +67,9 @@ def linear_d(input_, output_size,scope=None, with_w=False):
         else:
             return tf.nn.bias_add(tf.matmul(input_, matrix), bias)
 
-         
-         
+
+
+
 def deconv2d(input_, output_shape, name="deconv2d", with_w=False):
     with tf.variable_scope(name):
         # filter : [height, width, output_channels, in_channels]
@@ -86,8 +84,6 @@ def deconv2d(input_, output_shape, name="deconv2d", with_w=False):
 
 
 
-
-
 # In[2]:  # read image form pickle file
 
 def unpickle(file):
@@ -97,73 +93,98 @@ def unpickle(file):
     elif sys.version_info.major == 3:
         data = pickle.load(fp, encoding='latin-1')
     fp.close()
+
     return data
 
 
-
-img_batch= unpickle("62_seen_batch.pickle")
-X_image=np.array(img_batch)/ 255
-# shape=(batch_size, 64, 64, 3)
+test= unpickle("train_image.pickle")
 
 
-# In[8]:    discriminator, generator and sampler
+X_image=np.array(test)/127.5 - 1
+X_image.shape
 
-def discriminator(image):
-    batch_size=64
-    with tf.variable_scope("discriminator") as scope:
-        h0 = lrelu(conv2d(image, 64, name='d_h0_conv'))
-        h1 = lrelu(batch_norm(conv2d(h0, 128, name='d_h1_conv'),'d_bn1'))
-        h2 = lrelu(batch_norm(conv2d(h1, 256, name='d_h2_conv'),'d_bn2'))
-        h3 = lrelu(batch_norm(conv2d(h2, 512, name='d_h3_conv'),'d_bn3'))  # shape=(batch_size, 64, 64, 3)　
-        h4 = linear_d(tf.reshape(h3, [batch_size, -1]),2,'d_h4_lin')
+
+# In[3]: # discriminator, generator and sampler
+
+def discriminator():
+    reuse = False
+    def model(image):
+        nonlocal reuse
+        batch_size=64
+        with tf.variable_scope("discriminator", reuse=reuse) as scope:
+            h0 = lrelu(conv2d(image, 64, name='d_h0_conv'))
+            h1 = lrelu(batch_norm(conv2d(h0, 128, name='d_h1_conv'),'d_bn1'))
+            h2 = lrelu(batch_norm(conv2d(h1, 256, name='d_h2_conv'),'d_bn2'))
+            h3 = lrelu(batch_norm(conv2d(h2, 512, name='d_h3_conv'),'d_bn3'))  # shape=(batch_size, 64, 64, 3)　
+            h4 = linear_d(tf.reshape(h3, [batch_size, -1]),2,'d_h4_lin')
+        reuse = True
         return h4
+    return model
     
 
                     
     # shape=(batch_size, 64, 64, 3)
-def generator(z_):
-    batch_size=64
-    with tf.variable_scope("generator") as scope:
-        # project `z` and reshape
-        z, h0_w, h0_b = linear(z_, 64*8*4*4, 'g_h0_lin',with_w=True)
-        h0 = tf.nn.relu(batch_norm(tf.reshape(z, [-1, 4, 4, 64*8]), 'g_bn0'))
-        h1, h1_w, h1_b = deconv2d(h0, [batch_size, 8, 8, 64*4], name='g_h1', with_w=True)
-        h1 = tf.nn.relu(batch_norm(h1, 'g_bn1'))
-        h2, h2_w, h2_b = deconv2d(h1, [batch_size, 16, 16, 64*2], name='g_h2', with_w=True)
-        h2 = tf.nn.relu(batch_norm(h2, 'g_bn2'))
-        h3, h3_w, h3_b = deconv2d(h2, [batch_size, 32, 32, 64*1], name='g_h3', with_w=True)
-        h3 = tf.nn.relu(batch_norm(h3, 'g_bn3'))
-        h4, h4_w, h4_b = deconv2d(h3, [batch_size, 64, 64, 3], name='g_h4', with_w=True)
+def generator():
+    reuse = False
+    def model(z_):
+        nonlocal reuse
+        batch_size=64
+        with tf.variable_scope("generator", reuse=reuse) as scope:
+            # project `z` and reshape
+            z, h0_w, h0_b = linear(z_, 64*8*4*4, 'g_h0_lin',with_w=True)
+            h0 = tf.nn.relu(batch_norm(tf.reshape(z, [-1, 4, 4, 64*8]), 'g_bn0'))
+            h1, h1_w, h1_b = deconv2d(h0, [batch_size, 8, 8, 64*4], name='g_h1', with_w=True)
+            h1 = tf.nn.relu(batch_norm(h1, 'g_bn1'))
+            h2, h2_w, h2_b = deconv2d(h1, [batch_size, 16, 16, 64*2], name='g_h2', with_w=True)
+            h2 = tf.nn.relu(batch_norm(h2, 'g_bn2'))
+            h3, h3_w, h3_b = deconv2d(h2, [batch_size, 32, 32, 64*1], name='g_h3', with_w=True)
+            h3 = tf.nn.relu(batch_norm(h3, 'g_bn3'))
+            h4, h4_w, h4_b = deconv2d(h3, [batch_size, 64, 64, 3], name='g_h4', with_w=True)
+        reuse = True
         return tf.nn.tanh(h4)  #shape=(batch_size, 64, 64, 3)
+    return model
 
 
-def sampler(z_):# shape=(batch_size, 64, 64, 3)　
-    batch_size=64
-    with tf.variable_scope("generator") as scope:
-        # project `z` and reshape
-        z= linear(z_, 64*8*4*4,'g_h0_lin')
-        h0 = tf.nn.relu(batch_norm(tf.reshape(z, [-1, 4, 4, 64*8]),'g_bn0',train=False))
-        h1 = deconv2d(h0, [batch_size, 8, 8, 64*4], name='g_h1')
-        h1 = tf.nn.relu(batch_norm(h1,'g_bn1',train=False))
-        h2 = deconv2d(h1, [batch_size, 16, 16, 64*2], name='g_h2')
-        h2 = tf.nn.relu(batch_norm(h2,'g_bn2',train=False))
-        h3 = deconv2d(h2, [batch_size, 32, 32, 64*1], name='g_h3')
-        h3 = tf.nn.relu(batch_norm(h3,'g_bn3',train=False))
-        h4 = deconv2d(h3, [batch_size, 64, 64, 3], name='g_h4')
+def sampler():# shape=(batch_size, 64, 64, 3)　
+    reuse = True
+    def model(z_):
+        nonlocal reuse
+        batch_size=64
+        with tf.variable_scope("generator",reuse=reuse) as scope:
+            # project `z` and reshape
+            z= linear(z_, 64*8*4*4,'g_h0_lin')
+            h0 = tf.nn.relu(batch_norm(tf.reshape(z, [-1, 4, 4, 64*8]),'g_bn0',train=False))
+            h1 = deconv2d(h0, [batch_size, 8, 8, 64*4], name='g_h1')
+            h1 = tf.nn.relu(batch_norm(h1,'g_bn1',train=False))
+            h2 = deconv2d(h1, [batch_size, 16, 16, 64*2], name='g_h2')
+            h2 = tf.nn.relu(batch_norm(h2,'g_bn2',train=False))
+            h3 = deconv2d(h2, [batch_size, 32, 32, 64*1], name='g_h3')
+            h3 = tf.nn.relu(batch_norm(h3,'g_bn3',train=False))
+            h4 = deconv2d(h3, [batch_size, 64, 64, 3], name='g_h4')
         return tf.nn.tanh(h4)  #shape=(batch_size, 64, 64, 3)
-
-G=generator(z)  #G(z)
-D_logits = discriminator(image) #D(x)
+    return model
 
 
-# In[9]:
-
-tf.get_variable_scope().reuse_variables()
-sampler = sampler(z)
-D_logits_ = discriminator(G)   #D(G(z))
+g = generator()
+d = discriminator()
+s = sampler()
 
 
-# In[10]: # loss function
+
+# In[4]:
+
+G=g(z)  #G(z)
+D_logits = d(image) #D(x)
+D_logits_ = d(G)   #D(G(z))
+
+
+# In[5]:
+
+sampler = s(z)
+
+
+# In[5]:  # loss function
+
 batch_label=64
 
 d_loss_real = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=D_logits, labels=tf.ones([batch_label], dtype=tf.int64)))
@@ -173,17 +194,21 @@ g_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=D_
 d_loss = d_loss_real + d_loss_fake
 
 
-# In[11]:  # optim
+
+# In[6]: # optim
 
 d_vars = [var for var in tf.trainable_variables() if 'd_' in var.name]
 g_vars = [var for var in tf.trainable_variables() if 'g_' in var.name]
 
-d_optim = tf.train.GradientDescentOptimizer(0.0001).minimize(d_loss, var_list=d_vars)
-g_optim = tf.train.GradientDescentOptimizer(0.0001).minimize(g_loss, var_list=g_vars)
+saver=tf.train.Saver()
+
+
+g_optim = tf.train.AdamOptimizer(learning_rate=0.0002, beta1=0.5).minimize(g_loss, var_list=g_vars)
+d_optim = tf.train.AdamOptimizer(learning_rate=0.0002, beta1=0.5).minimize(d_loss, var_list=d_vars)
 
 
 
-# In[13]:    # train
+# In[10]:  # train
 
 run_config = tf.ConfigProto()
 run_config.gpu_options.allow_growth=True
@@ -195,7 +220,7 @@ with tf.Session(config=run_config) as sess:
     sample_images = np.array(sample).astype(np.float32)
     
     counter=1
-    epochs=1
+    epochs=3
     start_time=time.time()
     for epoch in range(epochs):
         batch_idxs= min (len(X_image), np.inf) // 64
@@ -215,7 +240,6 @@ with tf.Session(config=run_config) as sess:
             counter += 1
             print("Epoch: [%2d] [%4d/%4d] time:%4.4f, d_loss: %.8f, g_loss: %.8f" % (epoch, idx, batch_idxs,
                                                                                          time.time()-start_time, errD_fake+errD_real, errG))
-            
             # show sample image while traing
             if np.mod(counter, 30)==1:
                 samples, d_loss_sample, g_loss_sample = sess.run([sampler, d_loss, g_loss],
@@ -229,4 +253,4 @@ with tf.Session(config=run_config) as sess:
                 vnari=np.vstack(rows)
                 plt.imshow(vnari)
                 plt.show()
-                
+
